f68e237027a7a588ef05b0e497115222af768047
Andaz Kumar <andazkumar46@gmail.com>
2025-06-20 12:20:59 +0530

    Added New Files

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,50 @@
+// +++++++++++++++ Numbers +++++++++++++++++++++++++
+const score = 400
+// console.log(score);
+
+const balance = new Number(100.00)
+// console.log(balance);
+// console.log(balance.toString().length); // 3
+// console.log(balance.toFixed(2)); // 100.00
+
+const otherNumber = 123.5168
+
+// console.log(otherNumber.toPrecision(4)); // 123.5
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++ Maths +++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // 4
+// console.log(Math.round(4.4)); // 4
+// console.log(Math.round(4.5)); // 5
+// console.log(Math.ceil(4.1)); // 5 
+// console.log(Math.floor(4.9)); // 4
+// console.log(Math.sqrt(25)); // 5
+// console.log(Math.pow(5,2)); //25
+// console.log(Math.min(2,8,4,7,9,5,8)); // 2
+// console.log(Math.max(2,8,4,7,9,5,8)); // 9
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor((Math.random()*10) +1));
+
+const min = 20
+const max = 30
+// console.log((Math.random() * (max - min +1)) + min);
+console.log(Math.floor((Math.random() * (max - min +1)) + min));
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/test.js
@@ -0,0 +1,20 @@
+// console.log("Hello andaz welocme to javascript");
+a = "1" +2;
+b = 1 +"3"
+e = 1 -"3"
+f = "3" -1
+c = 2 * "4"
+d = 5 /"3"
+console.log(a);
+console.log(typeof a);
+console.log(b);
+console.log(typeof b);
+console.log(c);
+console.log(typeof c);
+console.log(d);
+console.log(typeof d);
+console.log(e);
+console.log(typeof e);
+console.log(f);
+console.log(typeof f);
+
