8f62be3b0c4edf50f7a6cc6c38ff938c1ce990ca
Andaz Kumar <andazkumar46@gmail.com>
2025-06-20 20:34:12 +0530

    some changed

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -9,6 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
+//++++++++++  spread operator (...)  ++++++++++++++++++++
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
@@ -16,16 +17,16 @@ const all_new_heros = [...marvel_heros, ...dc_heros]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+// console.log(Array.isArray("Andaz"))  // false
+// console.log(Array.from("Andaz"))  //  [ 'A', 'n', 'd', 'a', 'z' ]
+// console.log(Array.from({name: "hitesh"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));  //[ 100, 200, 300 ]
